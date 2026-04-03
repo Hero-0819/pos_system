55f986cd1ceb56fa5b669c5e8f179a754bd4146a
shujaa <cassianshujaa06@gmail.com>
2026-04-03 05:36:41 -0400

    fix STATIC_ROOT for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/login/'
