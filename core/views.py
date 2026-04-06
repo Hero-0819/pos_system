bea9a7f49ef697da39eac0497ebaa419ee1c2749
shujaa <cassianshujaa06@gmail.com>
2026-04-06 06:22:52 -0400

    reset admin for production

--- core/views.py
+++ core/views.py
@@ -13,7 +13,7 @@ from decimal import Decimal
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 def create_admin(request):
-    if not User.objects.filter(username="admin").delete()
+        User.objects.filter(username="admin").delete()
         User.objects.create_superuser(
             username="admin",
             email="cassianshujaa06@gmail.com",
