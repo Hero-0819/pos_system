77fa4b5f308d29a1ae210d9bcdc26d5fb749e90d
SHUJAA CASSIAN <130820785+Hero-0819@users.noreply.github.com>
2026-04-13 13:50:09 -0700

    Add STATICFILES_DIRS for static file configuration

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
